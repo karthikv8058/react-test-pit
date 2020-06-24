705b9e9d8ff3d7f1f0e2073f46d6a7c25dbc5fef
unknown <karthikv8058>
2020-06-24 10:53:08 +0530

    first commit

---
+++ src/Containers/Screen2/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Button from '../../Components/Button';
+import './style.css';
+
+class Screen2 extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div className="row text-dark">
+        <div className="col-sm-4">
+          for uploading img
+        </div>
+        <div className="col-sm-8">
+          for form data
+        </div>
+      </div>
+    );
+  }
+}
+export default Screen2;
