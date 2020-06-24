0571542421232017f946d51d3ba1aee00937c041
karthikv8058 <karthik.v.work@gmail.com>
2020-06-24 10:45:06 +0530

    2 files modified

--- Containers/Screen1/index.js
+++ Containers/Screen1/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Button from '../../Components/Button';
+import {withRouter} from 'react-router-dom'
 import Screen2 from '../Screen2';
 import './style.css';
 import history from '../../history';
@@ -17,10 +18,10 @@ class Screen1 extends Component {
     return (
       <div className="d-flex h-100vh justify-content-center">
         <div className="align-self-center">
-          <button className="btn-screen1" onClick={() => history.push('/Screen2')}>Register</button>
+          <button className="btn-screen1" onClick={() =>  this.props.history.push('/Screen2')}>Register</button>
         </div>
       </div>
     );
   }
 }
-export default Screen1;
+export default withRouter(Screen1);
--- Routes.js
+++ Routes.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Screen1 from './Containers/Screen1';
 import Screen2 from './Containers/Screen2';
@@ -9,12 +9,12 @@ import history from './history';
 export default class Routes extends Component {
     render() {
         return (
-            <Router history={history}>
+            <BrowserRouter history={history}>
                 <Switch>
                     <Route path="/" exact component={Screen1} />
                     <Route path="/Screen2" component={Screen2} />
                 </Switch>
-            </Router>
+            </BrowserRouter>
         )
     }
 }
